a6fcd0719d60c900dd36997839f93bf499d448ee
Mihir Vyas <mihirv126@gmail.com>
2019-11-25 18:28:43 +0530

    Add files via upload

---
+++ RangedSelection/test/DateCell.swift
@@ -0,0 +1,9 @@
+import JTAppleCalendar
+import UIKit
+
+class DateCell: JTAppleCell {
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var selectedView: UIView!
+    @IBOutlet weak var dotView: UIView!
+    
+}
---
+++ RangedSelection/test/Months.swift
@@ -0,0 +1,25 @@
+//
+//  Months.swift
+//  test
+//
+//  Created by Owebest MV on 25/11/19.
+//  Copyright © 2019 AppleInc. All rights reserved.
+//
+
+import Foundation
+
+//enum for months
+enum Month {
+  case January
+  case February
+  case March
+  case Aprail
+  case May
+  case June
+  case July
+  case August
+  case September
+  case October
+  case November
+  case December
+}
